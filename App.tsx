5c421b91b55ad9eb4419a73fb73729ddddd38b23
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-08-07 16:49:57 -0500

    first build of app kinda working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [yourAge, setYourAge] = useState("");
+  const [theirAge, setTheirAge] = useState("");
+  const [halfTime, setHalfTime] = useState(-1);
+
+  function cleanAndSetAge(setter: (arg0: string) => void, text: string) {
+    const sanitizedText: string = text.replace(/[^\d]/g, "");
+    if (null != sanitizedText) {
+      setter(sanitizedText);
+    }
+  }
+
+  useEffect(
+    function calculateHalfTime(): void {
+      const i: number = parseInt(yourAge);
+      const u: number = parseInt(theirAge);
+      const h: number = !isNaN(i) && !isNaN(u) ? u - i : -1;
+      setHalfTime(h);
+    },
+    [yourAge, theirAge]
+  );
+
+  function renderEncouragement() {
+    let styleName: any = styles.standardMessage;
+    let message: string = "OK";
+
+    if (halfTime === -1) {
+      message = "Not sure how?";
+    } else if (halfTime < +yourAge) {
+      styleName = styles.missed;
+      message = "Aww... you missed it!";
+    } else {
+      const yearsToGo: number = halfTime - parseInt(yourAge);
+      if (yearsToGo === 0) {
+        message = "Your time is now!";
+      } else {
+        message = `You only have ${yearsToGo} year${
+          1 === yearsToGo ? "" : "s"
+        } to go!`;
+      }
+    }
+    return <Text style={styleName}>{message}</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        style={styles.inputs}
+        placeholder="Your age"
+        onChangeText={text => cleanAndSetAge(setYourAge, text)}
+        defaultValue={yourAge}
+      />
+      <TextInput
+        style={styles.inputs}
+        placeholder="Their age"
+        onChangeText={text => cleanAndSetAge(setTheirAge, text)}
+        defaultValue={theirAge}
+      />
+      <Text style={styles.result}>{halfTime}</Text>
+      {renderEncouragement() || <Text>Wow</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +70,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  inputs: {
+    borderWidth: 1,
+    fontSize: 32,
+    height: 110,
+    marginBottom: 15,
+    width: "90%",
+    textAlign: "center"
+  },
+  result: {
+    fontSize: 48
+  },
+  missed: {
+    color: "#C86298"
+  },
+  standardMessage: {
+    color: "#443FA3"
+  }
 });
